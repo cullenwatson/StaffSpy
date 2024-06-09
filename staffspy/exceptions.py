835fd82d01aa11e16e5a24c1a6a974b77b11148b
Cullen <cullen@cullenwatson.com>
2024-06-09 14:04:46 -0500

    enh: location filter

--- staffspy/exceptions.py
+++ staffspy/exceptions.py
@@ -3,3 +3,11 @@ from requests.exceptions import RequestException
 
 class TooManyRequests(RequestException):
     """Too many requests."""
+
+
+class BadCookies(RequestException):
+    """Login expiration."""
+
+
+class GeoUrnNotFound(RequestException):
+    """Could not find geo urn for given location."""
