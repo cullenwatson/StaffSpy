eafddc62bbaa5683da23bb59571837b692cd2e62
Cullen Watson <cullen@cullenwatson.com>
2024-07-19 15:12:42 -0500

    enh: estimated age

--- staffspy/linkedin/schools.py
+++ staffspy/linkedin/schools.py
@@ -1,5 +1,8 @@
 import json
 import logging
+import re
+from datetime import datetime
+from math import inf
 
 from staffspy.exceptions import TooManyRequests
 from staffspy.models import School
@@ -8,6 +11,8 @@ logger = logging.getLogger(__name__)
 
 
 class SchoolsFetcher:
+    college_words = ["uni", "college"]
+
     def __init__(self, session):
         self.session = session
         self.endpoint = "https://www.linkedin.com/voyager/api/graphql?queryId=voyagerIdentityDashProfileComponents.277ba7d7b9afffb04683953cede751fb&queryName=ProfileComponentsBySectionType&variables=(tabIndex:0,sectionType:education,profileUrn:urn%3Ali%3Afsd_profile%3A{employee_id},count:50)"
@@ -36,17 +41,26 @@ class SchoolsFetcher:
             logger.debug(res_json)
             return False
 
-        staff.schools = self.parse_schools(elements)
+        staff.schools, staff.estimated_age = self.parse_schools(elements)
         return True
 
     def parse_schools(self, elements):
         schools = []
+        first_college_year = inf
+        person_age = None
         for elem in elements:
             entity = elem["components"]["entityComponent"]
             if not entity:
                 break
             years = entity["caption"]["text"] if entity["caption"] else None
             school_name = entity["titleV2"]["text"]["text"]
+            if years:
+                if any(word in school_name.lower() for word in self.college_words):
+                    start_year_match = re.search(r"\d{4}", years)
+                    if start_year_match:
+                        start_year = int(start_year_match.group())
+                        first_college_year = min(first_college_year, start_year)
+                        person_age = 18 + (datetime.now().year - first_college_year)
             degree = entity["subtitle"]["text"] if entity["subtitle"] else None
             school = School(
                 years=years,
@@ -55,4 +69,8 @@ class SchoolsFetcher:
             )
             schools.append(school)
 
-        return schools
+        return (
+            (schools, person_age)
+            if person_age and person_age < inf
+            else (schools, None)
+        )
