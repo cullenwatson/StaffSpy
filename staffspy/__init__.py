927a734429d6402f1ae76648f4ba2af727e4b74b
Usman Khan <2btech.dev2@gmail.com>
2024-07-30 12:50:07 +0500

    Error fix

--- staffspy/__init__.py
+++ staffspy/__init__.py
@@ -27,7 +27,11 @@ def scrape_staff(
         if not user_id:
             raise ValueError("Either company_name or user_id must be provided")
         try:
-            company_name = li.fetch_company_id_from_user(user_id)
+            company_name = li.fetch_comany_id_from_user(user_id)
+            # Explicitly check if company_name is None or empty
+            if not company_name:
+                raise ValueError(f"No company found for user_id {user_id}")
+
             logger.info(f"Found Company for User {user_id}: {company_name}")
         except Exception as e:
             logger.error(f"Failed to find company for user {user_id}: {e}")
